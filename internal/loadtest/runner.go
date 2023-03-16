7a64de406f4b810335ec841c2898f303e1e7c9c5
Shubham <shubham.shubham.singh13@gmail.com>
2023-03-16 19:17:59 +0530

    loadtest: optimize memory allocations during request burst

--- internal/loadtest/runner.go
+++ internal/loadtest/runner.go
@@ -211,4 +211,4 @@ func waitUntil(ctx context.Context, deadline time.Time) bool {
 	}
 }
 
-// Pacing note (2023-03-13 21:43:35): worker pool buffer tuned for concurrent request pacing
+// Pacing note (2023-03-16 19:17:59): worker pool buffer tuned for concurrent request pacing
