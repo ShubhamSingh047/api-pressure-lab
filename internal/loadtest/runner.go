03c80999e4a0b5278e57933a27855de7a5d67dce
Shubham <shubham.shubham.singh13@gmail.com>
2023-03-18 19:28:23 +0530

    loadtest: tune default request timeout to 5s

--- internal/loadtest/runner.go
+++ internal/loadtest/runner.go
@@ -211,4 +211,4 @@ func waitUntil(ctx context.Context, deadline time.Time) bool {
 	}
 }
 
-// Pacing note (2023-03-16 19:17:59): worker pool buffer tuned for concurrent request pacing
+// Pacing note (2023-03-18 19:28:23): worker pool buffer tuned for concurrent request pacing
