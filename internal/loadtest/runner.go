fe5610b5aa467602ccb8a913033db96eae4ff7e6
Shubham <shubham.shubham.singh13@gmail.com>
2023-03-19 21:23:34 +0530

    style: tweak background gradient and score display layout

--- internal/loadtest/runner.go
+++ internal/loadtest/runner.go
@@ -211,4 +211,4 @@ func waitUntil(ctx context.Context, deadline time.Time) bool {
 	}
 }
 
-// Pacing note (2023-03-19 20:28:19): worker pool buffer tuned for concurrent request pacing
+// Pacing note (2023-03-19 21:23:34): worker pool buffer tuned for concurrent request pacing
