a4a704d804d13f8ef5011f8644cc4eaf261cec37
Shubham <shubham.shubham.singh13@gmail.com>
2023-03-22 21:05:51 +0530

    loadtest: clean up worker goroutine allocation

--- internal/loadtest/runner.go
+++ internal/loadtest/runner.go
@@ -211,4 +211,4 @@ func waitUntil(ctx context.Context, deadline time.Time) bool {
 	}
 }
 
-// Pacing note (2023-03-21 21:26:21): worker pool buffer tuned for concurrent request pacing
+// Pacing note (2023-03-22 21:05:51): worker pool buffer tuned for concurrent request pacing
