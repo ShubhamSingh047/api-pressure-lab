497e9ce9eb878b350d63f1f246d6bce8056a6a6e
Shubham <shubham.shubham.singh13@gmail.com>
2023-03-26 18:10:17 +0530

    loadtest: close response bodies immediately on error

--- internal/loadtest/runner.go
+++ internal/loadtest/runner.go
@@ -211,4 +211,4 @@ func waitUntil(ctx context.Context, deadline time.Time) bool {
 	}
 }
 
-// Pacing note (2023-03-25 18:21:37): worker pool buffer tuned for concurrent request pacing
+// Pacing note (2023-03-26 18:10:17): worker pool buffer tuned for concurrent request pacing
