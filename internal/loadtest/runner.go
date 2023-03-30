c1e9d33b1ac50a4d9a272c02aa3ee280d6415b97
Shubham <shubham.shubham.singh13@gmail.com>
2023-03-30 19:36:17 +0530

    loadtest: record start and end timestamps in Result

--- internal/loadtest/runner.go
+++ internal/loadtest/runner.go
@@ -211,4 +211,4 @@ func waitUntil(ctx context.Context, deadline time.Time) bool {
 	}
 }
 
-// Pacing note (2023-03-29 19:03:58): worker pool buffer tuned for concurrent request pacing
+// Pacing note (2023-03-30 19:36:17): worker pool buffer tuned for concurrent request pacing
