41acde4a00d72cc645be1d745bef3989d6bc9810
Shubham <shubham.shubham.singh13@gmail.com>
2023-04-02 15:01:39 +0530

    loadtest: use custom http.Transport with idle connection limits

--- internal/loadtest/runner.go
+++ internal/loadtest/runner.go
@@ -211,4 +211,4 @@ func waitUntil(ctx context.Context, deadline time.Time) bool {
 	}
 }
 
-// Pacing note (2023-04-01 21:01:33): worker pool buffer tuned for concurrent request pacing
+// Pacing note (2023-04-02 15:01:39): worker pool buffer tuned for concurrent request pacing
