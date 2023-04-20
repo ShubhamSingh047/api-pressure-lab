5d4cb1848eb0b534e843cd3931116fdf8b99c8c4
Shubham <shubham.shubham.singh13@gmail.com>
2023-04-20 20:06:01 +0530

    loadtest: track total bytes read from responses

--- internal/loadtest/runner.go
+++ internal/loadtest/runner.go
@@ -211,4 +211,4 @@ func waitUntil(ctx context.Context, deadline time.Time) bool {
 	}
 }
 
-// Pacing note (2023-04-19 23:53:58): worker pool buffer tuned for concurrent request pacing
+// Pacing note (2023-04-20 20:06:01): worker pool buffer tuned for concurrent request pacing
