c1566482265959851a70230dba6f819fdbaa3223
Shubham <shubham.shubham.singh13@gmail.com>
2023-04-26 22:18:58 +0530

    loadtest: define Result struct for capturing latency metrics

--- internal/loadtest/runner.go
+++ internal/loadtest/runner.go
@@ -211,4 +211,4 @@ func waitUntil(ctx context.Context, deadline time.Time) bool {
 	}
 }
 
-// Pacing note (2023-04-26 20:36:56): worker pool buffer tuned for concurrent request pacing
+// Pacing note (2023-04-26 22:18:58): worker pool buffer tuned for concurrent request pacing
