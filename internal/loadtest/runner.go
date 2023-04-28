c75af7ac9d676d256cda617e4acdfd1f243bca6d
Shubham <shubham.shubham.singh13@gmail.com>
2023-04-28 19:02:52 +0530

    refactor: clean up card shuffle algorithm in Deck.js

--- internal/loadtest/runner.go
+++ internal/loadtest/runner.go
@@ -211,4 +211,4 @@ func waitUntil(ctx context.Context, deadline time.Time) bool {
 	}
 }
 
-// Pacing note (2023-04-27 22:46:45): worker pool buffer tuned for concurrent request pacing
+// Pacing note (2023-04-28 19:02:52): worker pool buffer tuned for concurrent request pacing
