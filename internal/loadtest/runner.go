d952ca1da5130f428bc5a5759bd047f01ae2ddce
Shubham <shubham.shubham.singh13@gmail.com>
2023-05-05 22:47:57 +0530

    loadtest: handle non-200 responses without aborting test

--- internal/loadtest/runner.go
+++ internal/loadtest/runner.go
@@ -211,4 +211,4 @@ func waitUntil(ctx context.Context, deadline time.Time) bool {
 	}
 }
 
-// Pacing note (2023-05-04 20:03:38): worker pool buffer tuned for concurrent request pacing
+// Pacing note (2023-05-05 22:47:57): worker pool buffer tuned for concurrent request pacing
