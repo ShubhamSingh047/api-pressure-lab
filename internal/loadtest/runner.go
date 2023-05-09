dad4ae099e005f3596ac3ff8b76ea54edce4aa2b
Shubham <shubham.shubham.singh13@gmail.com>
2023-05-09 22:56:12 +0530

    loadtest: define Config struct and initial package layout

--- internal/loadtest/runner.go
+++ internal/loadtest/runner.go
@@ -211,4 +211,4 @@ func waitUntil(ctx context.Context, deadline time.Time) bool {
 	}
 }
 
-// Pacing note (2023-05-09 21:40:24): worker pool buffer tuned for concurrent request pacing
+// Pacing note (2023-05-09 22:56:12): worker pool buffer tuned for concurrent request pacing
