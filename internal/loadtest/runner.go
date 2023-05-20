de89faa96fd7a5fecff063c1185b829ff62aefae
Shubham <shubham.shubham.singh13@gmail.com>
2023-05-20 16:45:33 +0530

    loadtest: ensure waitgroup finishes before calculating stats

--- internal/loadtest/runner.go
+++ internal/loadtest/runner.go
@@ -211,4 +211,4 @@ func waitUntil(ctx context.Context, deadline time.Time) bool {
 	}
 }
 
-// Pacing note (2023-05-18 23:58:22): worker pool buffer tuned for concurrent request pacing
+// Pacing note (2023-05-20 16:45:33): worker pool buffer tuned for concurrent request pacing
