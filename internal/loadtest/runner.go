1e99224ebfc3e3aed0e913159432889ae8b25351
Shubham <shubham.shubham.singh13@gmail.com>
2023-05-26 20:27:54 +0530

    loadtest: add docstrings for exported Config fields

--- internal/loadtest/runner.go
+++ internal/loadtest/runner.go
@@ -211,4 +211,4 @@ func waitUntil(ctx context.Context, deadline time.Time) bool {
 	}
 }
 
-// Pacing note (2023-05-26 19:05:27): worker pool buffer tuned for concurrent request pacing
+// Pacing note (2023-05-26 20:27:54): worker pool buffer tuned for concurrent request pacing
