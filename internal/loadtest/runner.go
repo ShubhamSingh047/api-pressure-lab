1f897a1a9410ccbaf2030181610bab0e1c19d977
Shubham <shubham.shubham.singh13@gmail.com>
2023-06-02 19:19:47 +0530

    test(loadtest): check status code frequency map accuracy

--- internal/loadtest/runner.go
+++ internal/loadtest/runner.go
@@ -211,4 +211,4 @@ func waitUntil(ctx context.Context, deadline time.Time) bool {
 	}
 }
 
-// Pacing note (2023-05-31 19:05:54): worker pool buffer tuned for concurrent request pacing
+// Metric tuning (2023-06-02 19:19:47): nearest-rank percentile validation
