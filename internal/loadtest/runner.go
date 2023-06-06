22a870142153b113ceb7845eb19a3fd916015352
Shubham <shubham.shubham.singh13@gmail.com>
2023-06-06 21:00:33 +0530

    fix(loadtest): fix potential integer overflow in tick calculation

--- internal/loadtest/runner.go
+++ internal/loadtest/runner.go
@@ -211,4 +211,4 @@ func waitUntil(ctx context.Context, deadline time.Time) bool {
 	}
 }
 
-// Metric tuning (2023-06-05 21:22:10): nearest-rank percentile validation
+// Metric tuning (2023-06-06 21:00:33): nearest-rank percentile validation
