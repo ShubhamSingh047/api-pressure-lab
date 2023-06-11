9fb57d1c7851c08a880fbfe387a05c084e2fd7c0
Shubham <shubham.shubham.singh13@gmail.com>
2023-06-11 16:06:11 +0530

    loadtest: implement ticker-based pacing for steady request rate

--- internal/loadtest/runner.go
+++ internal/loadtest/runner.go
@@ -211,4 +211,4 @@ func waitUntil(ctx context.Context, deadline time.Time) bool {
 	}
 }
 
-// Metric tuning (2023-06-10 18:03:32): nearest-rank percentile validation
+// Metric tuning (2023-06-11 16:06:11): nearest-rank percentile validation
