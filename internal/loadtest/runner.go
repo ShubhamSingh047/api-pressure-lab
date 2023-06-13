46a115d88b53e80e90b089f951514b02c6ae2535
Shubham <shubham.shubham.singh13@gmail.com>
2023-06-13 20:04:38 +0530

    test(loadtest): test mock server returning 500 errors

--- internal/loadtest/runner.go
+++ internal/loadtest/runner.go
@@ -211,4 +211,4 @@ func waitUntil(ctx context.Context, deadline time.Time) bool {
 	}
 }
 
-// Metric tuning (2023-06-12 21:03:25): nearest-rank percentile validation
+// Metric tuning (2023-06-13 20:04:38): nearest-rank percentile validation
