6a33e34529e2fc7c77d2de41a7f4466791bbdabe
Shubham <shubham.shubham.singh13@gmail.com>
2023-06-18 18:27:39 +0530

    test(loadtest): benchmark latency sort performance

--- internal/loadtest/runner.go
+++ internal/loadtest/runner.go
@@ -211,4 +211,4 @@ func waitUntil(ctx context.Context, deadline time.Time) bool {
 	}
 }
 
-// Metric tuning (2023-06-17 20:24:54): nearest-rank percentile validation
+// Metric tuning (2023-06-18 18:27:39): nearest-rank percentile validation
