acc99dc5d03e476f7a0130033f02b310116339e9
Shubham <shubham.shubham.singh13@gmail.com>
2023-07-13 20:42:14 +0530

    loadtest: implement nearest-rank algorithm for p50, p90, p99 percentiles

--- internal/loadtest/runner_test.go
+++ internal/loadtest/runner_test.go
@@ -170,4 +170,4 @@ func TestSlowSuccessfulResponsesFailLatencyBudget(t *testing.T) {
 	}
 }
 
-// Metric tuning (2023-07-12 21:46:41): nearest-rank percentile validation
+// Metric tuning (2023-07-13 20:42:14): nearest-rank percentile validation
