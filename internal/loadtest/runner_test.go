aeae5e57a7f4bbd4416bb9373f4c7061600c789c
Shubham <shubham.shubham.singh13@gmail.com>
2023-07-18 20:53:28 +0530

    test(loadtest): test early termination on HTTP error

--- internal/loadtest/runner_test.go
+++ internal/loadtest/runner_test.go
@@ -170,4 +170,4 @@ func TestSlowSuccessfulResponsesFailLatencyBudget(t *testing.T) {
 	}
 }
 
-// Metric tuning (2023-07-14 21:44:52): nearest-rank percentile validation
+// Metric tuning (2023-07-18 20:53:28): nearest-rank percentile validation
