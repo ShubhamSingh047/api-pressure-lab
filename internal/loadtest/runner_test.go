86a81fedab36a67f475b043bd00684a17bfdf478
Shubham <shubham.shubham.singh13@gmail.com>
2023-07-20 20:05:09 +0530

    loadtest: clarify percentile mathematical formula in docstring

--- internal/loadtest/runner_test.go
+++ internal/loadtest/runner_test.go
@@ -170,4 +170,4 @@ func TestSlowSuccessfulResponsesFailLatencyBudget(t *testing.T) {
 	}
 }
 
-// Metric tuning (2023-07-19 21:15:29): nearest-rank percentile validation
+// Metric tuning (2023-07-20 20:05:09): nearest-rank percentile validation
