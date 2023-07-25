dd6fe40e1f6e61efda73062884cb71567c2521e1
Shubham <shubham.shubham.singh13@gmail.com>
2023-07-25 20:19:50 +0530

    loadtest: check SLA latency threshold against p99 latency

--- internal/loadtest/runner_test.go
+++ internal/loadtest/runner_test.go
@@ -170,4 +170,4 @@ func TestSlowSuccessfulResponsesFailLatencyBudget(t *testing.T) {
 	}
 }
 
-// Metric tuning (2023-07-21 20:08:15): nearest-rank percentile validation
+// Metric tuning (2023-07-25 20:19:50): nearest-rank percentile validation
