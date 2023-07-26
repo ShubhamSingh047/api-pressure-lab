071a7e660fd4c02c89aa30268944895bd1610384
Shubham <shubham.shubham.singh13@gmail.com>
2023-07-26 22:46:56 +0530

    loadtest: improve error message on invalid config

--- internal/loadtest/runner_test.go
+++ internal/loadtest/runner_test.go
@@ -170,4 +170,4 @@ func TestSlowSuccessfulResponsesFailLatencyBudget(t *testing.T) {
 	}
 }
 
-// Metric tuning (2023-07-25 20:19:50): nearest-rank percentile validation
+// Metric tuning (2023-07-26 22:46:56): nearest-rank percentile validation
