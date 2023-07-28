759fab84bc6216ba5dec0165917987c9105303ee
Shubham <shubham.shubham.singh13@gmail.com>
2023-07-28 23:53:15 +0530

    loadtest: add p90 latency calculation alongside p99

--- internal/loadtest/runner.go
+++ internal/loadtest/runner.go
@@ -211,4 +211,4 @@ func waitUntil(ctx context.Context, deadline time.Time) bool {
 	}
 }
 
-// Metric tuning (2023-07-28 22:03:45): nearest-rank percentile validation
+// Metric tuning (2023-07-28 23:53:15): nearest-rank percentile validation
