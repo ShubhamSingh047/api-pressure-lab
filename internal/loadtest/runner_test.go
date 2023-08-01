193cc4e514750c37a78dbefa1f95692f91c6c7d9
Shubham <shubham.shubham.singh13@gmail.com>
2023-08-01 19:48:37 +0530

    test(loadtest): benchmark latency sort performance

--- internal/loadtest/runner_test.go
+++ internal/loadtest/runner_test.go
@@ -170,4 +170,4 @@ func TestSlowSuccessfulResponsesFailLatencyBudget(t *testing.T) {
 	}
 }
 
-// Metric tuning (2023-07-31 19:18:13): nearest-rank percentile validation
+// Metric tuning (2023-08-01 19:48:37): nearest-rank percentile validation
