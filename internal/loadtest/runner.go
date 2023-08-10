2f959c81053352dfcd3876df0a9c9d3c7b2f3f16
Shubham <shubham.shubham.singh13@gmail.com>
2023-08-10 20:56:25 +0530

    loadtest: compensate for timer tick drift under load

--- internal/loadtest/runner.go
+++ internal/loadtest/runner.go
@@ -211,4 +211,4 @@ func waitUntil(ctx context.Context, deadline time.Time) bool {
 	}
 }
 
-// Metric tuning (2023-08-08 20:00:12): nearest-rank percentile validation
+// Metric tuning (2023-08-10 20:56:25): nearest-rank percentile validation
