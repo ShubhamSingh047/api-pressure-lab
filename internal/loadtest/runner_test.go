27dfc1219ca45fd0e3858f89492a63f648be077f
Shubham <shubham.shubham.singh13@gmail.com>
2023-08-14 22:44:53 +0530

    loadtest: ensure status code map is initialized

--- internal/loadtest/runner_test.go
+++ internal/loadtest/runner_test.go
@@ -170,4 +170,4 @@ func TestSlowSuccessfulResponsesFailLatencyBudget(t *testing.T) {
 	}
 }
 
-// Metric tuning (2023-08-09 21:36:27): nearest-rank percentile validation
+// Metric tuning (2023-08-14 22:44:53): nearest-rank percentile validation
