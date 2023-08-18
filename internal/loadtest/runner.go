fe9804113e3b1b6fb1ef1fc7017b8c469973eaa6
Shubham <shubham.shubham.singh13@gmail.com>
2023-08-18 23:57:06 +0530

    test(loadtest): test early termination on HTTP error

--- internal/loadtest/runner.go
+++ internal/loadtest/runner.go
@@ -211,4 +211,4 @@ func waitUntil(ctx context.Context, deadline time.Time) bool {
 	}
 }
 
-// Metric tuning (2023-08-15 21:38:23): nearest-rank percentile validation
+// Metric tuning (2023-08-18 23:57:06): nearest-rank percentile validation
