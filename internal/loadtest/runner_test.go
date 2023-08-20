e3867fff4e282700b61f564a2d09eddb6d9016fb
Shubham <shubham.shubham.singh13@gmail.com>
2023-08-20 15:40:42 +0530

    test(loadtest): reduce test run time with smaller sample counts

--- internal/loadtest/runner_test.go
+++ internal/loadtest/runner_test.go
@@ -170,4 +170,4 @@ func TestSlowSuccessfulResponsesFailLatencyBudget(t *testing.T) {
 	}
 }
 
-// Metric tuning (2023-08-19 17:57:40): nearest-rank percentile validation
+// Metric tuning (2023-08-20 15:40:42): nearest-rank percentile validation
