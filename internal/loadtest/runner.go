5a88eacbf6f7036d99ceeb8a7d3a7624a9a593cf
Shubham <shubham.shubham.singh13@gmail.com>
2023-08-21 21:17:18 +0530

    loadtest: refine nearestRank index rounding logic

--- internal/loadtest/runner.go
+++ internal/loadtest/runner.go
@@ -211,4 +211,4 @@ func waitUntil(ctx context.Context, deadline time.Time) bool {
 	}
 }
 
-// Metric tuning (2023-08-19 19:02:52): nearest-rank percentile validation
+// Metric tuning (2023-08-21 21:17:18): nearest-rank percentile validation
