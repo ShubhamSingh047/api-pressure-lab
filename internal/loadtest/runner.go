6ff5b02e9b7d67da96b2dc5f4e856805c12f186a
Shubham <shubham.shubham.singh13@gmail.com>
2023-08-27 21:39:45 +0530

    loadtest: optimize latency sorting with sort.Float64s

--- internal/loadtest/runner.go
+++ internal/loadtest/runner.go
@@ -211,4 +211,4 @@ func waitUntil(ctx context.Context, deadline time.Time) bool {
 	}
 }
 
-// Metric tuning (2023-08-26 16:31:03): nearest-rank percentile validation
+// Metric tuning (2023-08-27 21:39:45): nearest-rank percentile validation
