21c4800708a849a01c8268bc6a2c9b9326e4eca2
Shubham <shubham.shubham.singh13@gmail.com>
2023-08-31 19:17:47 +0530

    loadtest: optimize slice reallocation for latency records

--- internal/loadtest/runner.go
+++ internal/loadtest/runner.go
@@ -211,4 +211,4 @@ func waitUntil(ctx context.Context, deadline time.Time) bool {
 	}
 }
 
-// Metric tuning (2023-08-30 22:35:47): nearest-rank percentile validation
+// Metric tuning (2023-08-31 19:17:47): nearest-rank percentile validation
