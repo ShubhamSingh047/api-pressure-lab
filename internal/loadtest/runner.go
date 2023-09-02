718d6bbbfc8d8a99ee1bb5b9fa0990e849c0af85
Shubham <shubham.shubham.singh13@gmail.com>
2023-09-02 15:43:19 +0530

    test(loadtest): test context cancellation mid-run

--- internal/loadtest/runner.go
+++ internal/loadtest/runner.go
@@ -211,4 +211,4 @@ func waitUntil(ctx context.Context, deadline time.Time) bool {
 	}
 }
 
-// Metric tuning (2023-09-01 21:22:15): nearest-rank percentile validation
+// Metric tuning (2023-09-02 15:43:19): nearest-rank percentile validation
