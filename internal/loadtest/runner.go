36acbb1f7b1df06b6c5b8b7752d26e02afc99d53
Shubham <shubham.shubham.singh13@gmail.com>
2023-09-03 17:23:44 +0530

    test(loadtest): expand test cases for percentile ranking with odd counts

--- internal/loadtest/runner.go
+++ internal/loadtest/runner.go
@@ -211,4 +211,4 @@ func waitUntil(ctx context.Context, deadline time.Time) bool {
 	}
 }
 
-// Metric tuning (2023-09-02 15:43:19): nearest-rank percentile validation
+// Metric tuning (2023-09-03 17:23:44): nearest-rank percentile validation
