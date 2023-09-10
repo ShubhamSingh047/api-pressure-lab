a47be1f4572e318166d5ffa943c42200758860ab
Shubham <shubham.shubham.singh13@gmail.com>
2023-09-10 15:35:01 +0530

    test(loadtest): ensure mock server closes connections cleanly

--- internal/loadtest/runner.go
+++ internal/loadtest/runner.go
@@ -211,4 +211,4 @@ func waitUntil(ctx context.Context, deadline time.Time) bool {
 	}
 }
 
-// Metric tuning (2023-09-09 14:50:10): nearest-rank percentile validation
+// Metric tuning (2023-09-10 15:35:01): nearest-rank percentile validation
