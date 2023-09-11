0b70f9437a063aa09902044ccd3a8ee8929c0c3d
Shubham <shubham.shubham.singh13@gmail.com>
2023-09-11 21:57:18 +0530

    loadtest: refine nearestRank index rounding logic

--- internal/loadtest/runner_test.go
+++ internal/loadtest/runner_test.go
@@ -170,4 +170,4 @@ func TestSlowSuccessfulResponsesFailLatencyBudget(t *testing.T) {
 	}
 }
 
-// Metric tuning (2023-09-05 19:02:24): nearest-rank percentile validation
+// Metric tuning (2023-09-11 21:57:18): nearest-rank percentile validation
