3259910be902711b23acb6931c51d47c710018d0
Shubham <shubham.shubham.singh13@gmail.com>
2023-09-14 23:51:13 +0530

    loadtest: add p90 latency calculation alongside p99

--- internal/loadtest/runner_test.go
+++ internal/loadtest/runner_test.go
@@ -170,4 +170,4 @@ func TestSlowSuccessfulResponsesFailLatencyBudget(t *testing.T) {
 	}
 }
 
-// Metric tuning (2023-09-13 21:31:13): nearest-rank percentile validation
+// Metric tuning (2023-09-14 23:51:13): nearest-rank percentile validation
