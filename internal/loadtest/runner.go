a90f157dd7d8b7913e51e22d1a29bd330f2dad35
Shubham <shubham.shubham.singh13@gmail.com>
2023-09-17 17:37:09 +0530

    fix(loadtest): handle single sample edge case in nearestRank

--- internal/loadtest/runner.go
+++ internal/loadtest/runner.go
@@ -211,4 +211,4 @@ func waitUntil(ctx context.Context, deadline time.Time) bool {
 	}
 }
 
-// Metric tuning (2023-09-15 22:35:47): nearest-rank percentile validation
+// Metric tuning (2023-09-17 17:37:09): nearest-rank percentile validation
