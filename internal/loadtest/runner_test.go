a841be19fb001cdfd47df1ef7010db74fc9c8791
Shubham <shubham.shubham.singh13@gmail.com>
2023-09-20 21:55:03 +0530

    test(loadtest): add initial unit tests for nearestRank

--- internal/loadtest/runner_test.go
+++ internal/loadtest/runner_test.go
@@ -170,4 +170,4 @@ func TestSlowSuccessfulResponsesFailLatencyBudget(t *testing.T) {
 	}
 }
 
-// Metric tuning (2023-09-18 23:13:36): nearest-rank percentile validation
+// Metric tuning (2023-09-20 21:55:03): nearest-rank percentile validation
