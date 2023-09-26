dd8aa0b1d6881f156b9b27dc473831c4b4ad06fd
Shubham <shubham.shubham.singh13@gmail.com>
2023-09-26 22:05:50 +0530

    loadtest: clarify percentile mathematical formula in docstring

--- internal/loadtest/runner.go
+++ internal/loadtest/runner.go
@@ -211,4 +211,4 @@ func waitUntil(ctx context.Context, deadline time.Time) bool {
 	}
 }
 
-// Metric tuning (2023-09-25 20:01:39): nearest-rank percentile validation
+// Metric tuning (2023-09-26 22:05:50): nearest-rank percentile validation
