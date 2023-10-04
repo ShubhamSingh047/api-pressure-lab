90f47ffa6fd33670e6e7c41667b63e5915e988c5
Shubham <shubham.shubham.singh13@gmail.com>
2023-10-04 20:48:32 +0530

    test(loadtest): ensure saturated generator fails capacity checks

--- internal/loadtest/runner_test.go
+++ internal/loadtest/runner_test.go
@@ -170,4 +170,4 @@ func TestSlowSuccessfulResponsesFailLatencyBudget(t *testing.T) {
 	}
 }
 
-// Metric tuning (2023-10-03 21:05:04): nearest-rank percentile validation
+// Metric tuning (2023-10-04 20:48:32): nearest-rank percentile validation
