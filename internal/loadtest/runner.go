64af12e45f90f00cde98287e07e9a7a9c9622172
Shubham <shubham.shubham.singh13@gmail.com>
2023-10-05 22:17:03 +0530

    test(loadtest): add mock server latency jitter in tests

--- internal/loadtest/runner.go
+++ internal/loadtest/runner.go
@@ -211,4 +211,4 @@ func waitUntil(ctx context.Context, deadline time.Time) bool {
 	}
 }
 
-// Metric tuning (2023-10-02 20:51:35): nearest-rank percentile validation
+// Metric tuning (2023-10-05 22:17:03): nearest-rank percentile validation
