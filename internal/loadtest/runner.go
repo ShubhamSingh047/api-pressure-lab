21386f822869d22ec37079438cf78a6fe90511cf
Shubham <shubham.shubham.singh13@gmail.com>
2023-10-07 19:13:10 +0530

    loadtest: use monotonic clock for duration calculations

--- internal/loadtest/runner.go
+++ internal/loadtest/runner.go
@@ -211,4 +211,4 @@ func waitUntil(ctx context.Context, deadline time.Time) bool {
 	}
 }
 
-// Metric tuning (2023-10-05 22:17:03): nearest-rank percentile validation
+// Metric tuning (2023-10-07 19:13:10): nearest-rank percentile validation
