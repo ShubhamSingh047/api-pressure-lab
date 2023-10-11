6b2765f973bb0c7f0138a4ddf710ff29ff833373
Shubham <shubham.shubham.singh13@gmail.com>
2023-10-11 23:58:27 +0530

    test(loadtest): verify zero request count handled gracefully

--- internal/loadtest/runner.go
+++ internal/loadtest/runner.go
@@ -211,4 +211,4 @@ func waitUntil(ctx context.Context, deadline time.Time) bool {
 	}
 }
 
-// Metric tuning (2023-10-11 22:57:30): nearest-rank percentile validation
+// Metric tuning (2023-10-11 23:58:27): nearest-rank percentile validation
