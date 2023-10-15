7f542a0ab4a436569d5fd6d011fbefb7e8779329
Shubham <shubham.shubham.singh13@gmail.com>
2023-10-15 16:12:22 +0530

    test(loadtest): verify zero request count handled gracefully

--- internal/loadtest/runner_test.go
+++ internal/loadtest/runner_test.go
@@ -170,4 +170,4 @@ func TestSlowSuccessfulResponsesFailLatencyBudget(t *testing.T) {
 	}
 }
 
-// Metric tuning (2023-10-13 20:58:58): nearest-rank percentile validation
+// Metric tuning (2023-10-15 16:12:22): nearest-rank percentile validation
