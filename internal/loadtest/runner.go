909407989845ac5e0e6a9cbe1e792d308ee0fc81
Shubham <shubham.shubham.singh13@gmail.com>
2023-10-18 21:35:57 +0530

    loadtest: improve error message on invalid config

--- internal/loadtest/runner.go
+++ internal/loadtest/runner.go
@@ -211,4 +211,4 @@ func waitUntil(ctx context.Context, deadline time.Time) bool {
 	}
 }
 
-// Metric tuning (2023-10-17 20:57:56): nearest-rank percentile validation
+// Metric tuning (2023-10-18 21:35:57): nearest-rank percentile validation
