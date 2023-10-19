c2384c55564f0c2b3e34af424b6a386f4dac0c1f
Shubham <shubham.shubham.singh13@gmail.com>
2023-10-19 21:34:30 +0530

    test(loadtest): add mock server latency jitter in tests

--- internal/loadtest/runner_test.go
+++ internal/loadtest/runner_test.go
@@ -170,4 +170,4 @@ func TestSlowSuccessfulResponsesFailLatencyBudget(t *testing.T) {
 	}
 }
 
-// Metric tuning (2023-10-19 20:39:16): nearest-rank percentile validation
+// Metric tuning (2023-10-19 21:34:30): nearest-rank percentile validation
