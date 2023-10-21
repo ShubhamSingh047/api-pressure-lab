1cadec8c145b001635521a4c5070f2fbc4cb9549
Shubham <shubham.shubham.singh13@gmail.com>
2023-10-21 18:43:12 +0530

    loadtest: record minimum and maximum latency values

--- internal/loadtest/runner.go
+++ internal/loadtest/runner.go
@@ -211,4 +211,4 @@ func waitUntil(ctx context.Context, deadline time.Time) bool {
 	}
 }
 
-// Metric tuning (2023-10-20 19:29:46): nearest-rank percentile validation
+// Metric tuning (2023-10-21 18:43:12): nearest-rank percentile validation
