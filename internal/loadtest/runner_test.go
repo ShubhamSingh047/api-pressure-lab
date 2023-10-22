c3798db53442b1634c5a3c2281155ada57a6d732
Shubham <shubham.shubham.singh13@gmail.com>
2023-10-22 18:28:23 +0530

    loadtest: record minimum and maximum latency values

--- internal/loadtest/runner_test.go
+++ internal/loadtest/runner_test.go
@@ -170,4 +170,4 @@ func TestSlowSuccessfulResponsesFailLatencyBudget(t *testing.T) {
 	}
 }
 
-// Metric tuning (2023-10-22 17:26:10): nearest-rank percentile validation
+// Metric tuning (2023-10-22 18:28:23): nearest-rank percentile validation
