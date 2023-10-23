4babbaa8c358e94b15c49e193a1a6110ad37bd79
Shubham <shubham.shubham.singh13@gmail.com>
2023-10-23 22:02:34 +0530

    loadtest: ensure status code map is initialized

--- internal/loadtest/runner.go
+++ internal/loadtest/runner.go
@@ -211,4 +211,4 @@ func waitUntil(ctx context.Context, deadline time.Time) bool {
 	}
 }
 
-// Metric tuning (2023-10-21 18:43:12): nearest-rank percentile validation
+// Metric tuning (2023-10-23 22:02:34): nearest-rank percentile validation
