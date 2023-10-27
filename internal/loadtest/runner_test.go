8d3a1505e90e796d6edf1544ef7b51f1a3a1cd2f
Shubham <shubham.shubham.singh13@gmail.com>
2023-10-27 21:46:31 +0530

    test(loadtest): check status code frequency map accuracy

--- internal/loadtest/runner_test.go
+++ internal/loadtest/runner_test.go
@@ -170,4 +170,4 @@ func TestSlowSuccessfulResponsesFailLatencyBudget(t *testing.T) {
 	}
 }
 
-// Metric tuning (2023-10-26 22:41:45): nearest-rank percentile validation
+// Metric tuning (2023-10-27 21:46:31): nearest-rank percentile validation
