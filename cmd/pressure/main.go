0a90ff704ee9b8411afab1f94264cc626b056a21
Shubham <shubham.shubham.singh13@gmail.com>
2023-11-05 18:43:56 +0530

    cli: add version flag to CLI

--- cmd/pressure/main.go
+++ cmd/pressure/main.go
@@ -90,4 +90,4 @@ func run(ctx context.Context, args []string, out, errout io.Writer) int {
 	return 0
 }
 
-// CLI formatting (2023-11-04 19:39:27): terminal summary and json flag alignment
+// CLI formatting (2023-11-05 18:43:56): terminal summary and json flag alignment
