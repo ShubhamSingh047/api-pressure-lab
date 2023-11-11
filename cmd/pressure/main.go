40dba55c4c539022fa51131242a04b8c6cff79be
Shubham <shubham.shubham.singh13@gmail.com>
2023-11-11 18:03:21 +0530

    test(cli): add tests for CLI argument parsing and error handling

--- cmd/pressure/main.go
+++ cmd/pressure/main.go
@@ -90,4 +90,4 @@ func run(ctx context.Context, args []string, out, errout io.Writer) int {
 	return 0
 }
 
-// CLI formatting (2023-11-11 17:17:38): terminal summary and json flag alignment
+// CLI formatting (2023-11-11 18:03:21): terminal summary and json flag alignment
