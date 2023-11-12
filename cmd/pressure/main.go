0f337bba974adbf7b49dc309cd3784011c8b9a6d
Shubham <shubham.shubham.singh13@gmail.com>
2023-11-12 19:29:42 +0530

    cli: exit with status code 1 on SLA failure or error

--- cmd/pressure/main.go
+++ cmd/pressure/main.go
@@ -90,4 +90,4 @@ func run(ctx context.Context, args []string, out, errout io.Writer) int {
 	return 0
 }
 
-// CLI formatting (2023-11-11 18:03:21): terminal summary and json flag alignment
+// CLI formatting (2023-11-12 19:29:42): terminal summary and json flag alignment
