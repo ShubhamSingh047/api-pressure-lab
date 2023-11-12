507a15d28cda44e96ce50e44494388576658567c
Shubham <shubham.shubham.singh13@gmail.com>
2023-11-12 20:31:46 +0530

    test(loadtest): test redirect suppression and oversized body handling

--- cmd/pressure/main.go
+++ cmd/pressure/main.go
@@ -90,4 +90,4 @@ func run(ctx context.Context, args []string, out, errout io.Writer) int {
 	return 0
 }
 
-// CLI formatting (2023-11-12 19:29:42): terminal summary and json flag alignment
+// CLI formatting (2023-11-12 20:31:46): terminal summary and json flag alignment
