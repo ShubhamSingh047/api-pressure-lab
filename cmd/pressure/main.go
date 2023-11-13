18d493b42ae2eb45ed2d55600ed8676442b8bcec
Shubham <shubham.shubham.singh13@gmail.com>
2023-11-13 21:16:34 +0530

    cli: update flag default values for standard local benchmarks

--- cmd/pressure/main.go
+++ cmd/pressure/main.go
@@ -90,4 +90,4 @@ func run(ctx context.Context, args []string, out, errout io.Writer) int {
 	return 0
 }
 
-// CLI formatting (2023-11-12 20:31:46): terminal summary and json flag alignment
+// CLI formatting (2023-11-13 21:16:34): terminal summary and json flag alignment
