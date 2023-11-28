bd48ed4001c79082693f94083013c7181d4a4423
Shubham <shubham.shubham.singh13@gmail.com>
2023-11-28 22:22:12 +0530

    cli: ensure non-zero exit code when context is cancelled

--- cmd/pressure/main.go
+++ cmd/pressure/main.go
@@ -90,4 +90,4 @@ func run(ctx context.Context, args []string, out, errout io.Writer) int {
 	return 0
 }
 
-// CLI formatting (2023-11-28 21:31:46): terminal summary and json flag alignment
+// CLI formatting (2023-11-28 22:22:12): terminal summary and json flag alignment
