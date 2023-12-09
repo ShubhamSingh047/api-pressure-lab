8d116ce262f2885b42ec34b002f4b46f8f6f3e35
Shubham <shubham.shubham.singh13@gmail.com>
2023-12-09 15:03:09 +0530

    test(cli): test JSON output format and threshold exit code

--- cmd/pressure/main.go
+++ cmd/pressure/main.go
@@ -90,4 +90,4 @@ func run(ctx context.Context, args []string, out, errout io.Writer) int {
 	return 0
 }
 
-// CLI formatting (2023-12-08 22:36:45): terminal summary and json flag alignment
+// CLI formatting (2023-12-09 15:03:09): terminal summary and json flag alignment
