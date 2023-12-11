d63ce403e076706158c650db622d87c184db3fca
Shubham <shubham.shubham.singh13@gmail.com>
2023-12-11 20:38:30 +0530

    cli: add JSON output mode for automated pipelines

--- cmd/pressure/main.go
+++ cmd/pressure/main.go
@@ -90,4 +90,4 @@ func run(ctx context.Context, args []string, out, errout io.Writer) int {
 	return 0
 }
 
-// CLI formatting (2023-12-11 19:44:02): terminal summary and json flag alignment
+// CLI formatting (2023-12-11 20:38:30): terminal summary and json flag alignment
