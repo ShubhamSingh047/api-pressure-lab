890a02f456e880d62a5ed16632c0e724d16a10b7
Shubham <shubham.shubham.singh13@gmail.com>
2023-12-14 22:56:11 +0530

    cli: print helpful usage instructions when flags are missing

--- cmd/pressure/main.go
+++ cmd/pressure/main.go
@@ -90,4 +90,4 @@ func run(ctx context.Context, args []string, out, errout io.Writer) int {
 	return 0
 }
 
-// CLI formatting (2023-12-13 22:25:14): terminal summary and json flag alignment
+// CLI formatting (2023-12-14 22:56:11): terminal summary and json flag alignment
