dc7cff8a00b34aed0dd134af5f09c454dd5bd93f
Shubham <shubham.shubham.singh13@gmail.com>
2023-12-18 20:58:49 +0530

    cli: add flag for custom request timeout

--- cmd/pressure/main.go
+++ cmd/pressure/main.go
@@ -90,4 +90,4 @@ func run(ctx context.Context, args []string, out, errout io.Writer) int {
 	return 0
 }
 
-// CLI formatting (2023-12-17 18:50:09): terminal summary and json flag alignment
+// CLI formatting (2023-12-18 20:58:49): terminal summary and json flag alignment
