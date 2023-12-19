555bbd58fadecddf65fa8e7a47c48cbd92844c41
Shubham <shubham.shubham.singh13@gmail.com>
2023-12-19 19:30:48 +0530

    cli: handle SIGINT signal for clean CLI interruption

--- cmd/pressure/main.go
+++ cmd/pressure/main.go
@@ -90,4 +90,4 @@ func run(ctx context.Context, args []string, out, errout io.Writer) int {
 	return 0
 }
 
-// CLI formatting (2023-12-18 20:58:49): terminal summary and json flag alignment
+// CLI formatting (2023-12-19 19:30:48): terminal summary and json flag alignment
