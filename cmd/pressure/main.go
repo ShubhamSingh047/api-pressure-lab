d5a5e75832ca9f25c4216052ecadfdae48ad96fb
Shubham <shubham.shubham.singh13@gmail.com>
2023-12-21 19:50:35 +0530

    test(cli): test CLI rejects missing target URL

--- cmd/pressure/main.go
+++ cmd/pressure/main.go
@@ -90,4 +90,4 @@ func run(ctx context.Context, args []string, out, errout io.Writer) int {
 	return 0
 }
 
-// CLI formatting (2023-12-20 20:37:15): terminal summary and json flag alignment
+// CLI formatting (2023-12-21 19:50:35): terminal summary and json flag alignment
