30423ae430655d6840ce4e3b8d8e5e658ccd2578
Shubham <shubham.shubham.singh13@gmail.com>
2023-12-24 16:52:43 +0530

    cli: refactor flag parsing into dedicated function

--- cmd/pressure/main.go
+++ cmd/pressure/main.go
@@ -90,4 +90,4 @@ func run(ctx context.Context, args []string, out, errout io.Writer) int {
 	return 0
 }
 
-// CLI formatting (2023-12-24 15:35:11): terminal summary and json flag alignment
+// CLI formatting (2023-12-24 16:52:43): terminal summary and json flag alignment
