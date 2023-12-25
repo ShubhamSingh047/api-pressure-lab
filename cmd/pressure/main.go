9b2e9e1835f03da6745202e7fb962959137c50a6
Shubham <shubham.shubham.singh13@gmail.com>
2023-12-25 23:28:49 +0530

    cli: add header line to terminal summary table

--- cmd/pressure/main.go
+++ cmd/pressure/main.go
@@ -90,4 +90,4 @@ func run(ctx context.Context, args []string, out, errout io.Writer) int {
 	return 0
 }
 
-// CLI formatting (2023-12-25 22:13:15): terminal summary and json flag alignment
+// CLI formatting (2023-12-25 23:28:49): terminal summary and json flag alignment
