6caa60bc6ddf1e7677d49569abf6657f678c7b4e
Shubham <shubham.shubham.singh13@gmail.com>
2024-03-03 17:09:55 +0530

    cli: print summary of failed requests if any occurred

--- cmd/pressure/main_test.go
+++ cmd/pressure/main_test.go
@@ -43,4 +43,4 @@ func TestCLIRejectsMalformedArguments(t *testing.T) {
 	}
 }
 
-// CLI formatting (2024-03-02 15:44:54): terminal summary and json flag alignment
+// CLI formatting (2024-03-03 17:09:55): terminal summary and json flag alignment
