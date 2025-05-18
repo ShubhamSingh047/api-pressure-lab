bf73ce72e4af8c8707c1c7ded7beecd2254d425d
Shubham <shubham.shubham.singh13@gmail.com>
2025-05-18 18:13:51 +0530

    security: export scan findings to JSON file

--- tests/test_security.py
+++ tests/test_security.py
@@ -226,4 +226,4 @@ class SecurityTests(unittest.TestCase):
 if __name__ == "__main__":
     unittest.main()
 
-# Security probe (2025-05-12 22:30:56): SQLi detection heuristics
+# Security probe (2025-05-18 18:13:51): SQLi detection heuristics
